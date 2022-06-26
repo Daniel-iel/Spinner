443915f154bf7b1b4bb239d1e2312120ef009969
Daniel <iel_182@hotmail.com>
2022-06-26 09:52:58 -0300

    add summary on PropertyNotMappedException

--- app/Spinner/Exceptions/PropertyNotMappedException.cs
+++ app/Spinner/Exceptions/PropertyNotMappedException.cs
@@ -4,11 +4,17 @@ using System.Text;
 
 namespace Spinner.Exceptions
 {
+    /// <summary>
+    /// The exception that is thrown when a property has no configuration attributes.
+    /// </summary>
     public class PropertyNotMappedException : Exception
     {
+        /// <summary>
+        /// Initializes a new instance of the PropertyNotMappedException class with a specified error
+        /// </summary>
+        /// <param name="message">The message that describes the error.</param>
         public PropertyNotMappedException(string message) : base(message)
         {
         }
-
     }
 }
