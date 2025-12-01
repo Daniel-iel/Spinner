4f27a2d1e831e7ee7a155f53101950d1d3051920
Daniel-iel <iel_182@hotmail.com>
2025-11-30 21:00:53 -0300

    Fix benchmark error

--- bench/Spinner.Benchmark/BenchmarkConfig.cs
+++ bench/Spinner.Benchmark/BenchmarkConfig.cs
@@ -10,14 +10,14 @@ namespace Writer.Benchmark
         public BenchmarkConfig()
         {
             AddDiagnoser(MemoryDiagnoser.Default);
-            
+
             // Adiciona CPUUsageDiagnoser apenas no Windows
             if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
             {
                 AddDiagnoser(new Microsoft.VSDiagnostics.CPUUsageDiagnoser());
             }
-            
-            AddJob(Job.Default.WithRuntime(BenchmarkDotNet.Environments.CoreRuntime.Core100).AsBaseline());
+
+            AddJob(Job.Default.WithRuntime(BenchmarkDotNet.Environments.CoreRuntime.Core10_0).AsBaseline());
             AddJob(Job.Default.WithRuntime(BenchmarkDotNet.Environments.CoreRuntime.Core90));
             AddJob(Job.Default.WithRuntime(BenchmarkDotNet.Environments.CoreRuntime.Core80));
         }
